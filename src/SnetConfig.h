45e2873df9d0f15687ec0f3745fc4a3e3eda8f3b
Florian Evers <florian-evers@gmx.de>
2016-11-22 23:14:52 +0100

    Changed version number to v1.1

--- src/SnetConfig.h
+++ src/SnetConfig.h
@@ -1,3 +1,3 @@
 // The configured options and settings for the snet-specific includes
 #define SNET_DEVEL_VERSION_MAJOR "1"
-#define SNET_DEVEL_VERSION_MINOR "1pre"
+#define SNET_DEVEL_VERSION_MINOR "1"
